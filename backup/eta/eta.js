ea09242e4b356125b6473a4fbf9c45c59ec7582f
20011662g <71864080+20011662g@users.noreply.github.com>
2021-09-12 00:33:09 +0800

    pwa fix, nav bar changed to sticky

--- backup/eta/eta.js
+++ backup/eta/eta.js
@@ -172,3 +172,20 @@ function renderETATables() {
 }
 
 
+// Popup for "Install to Home Screen"
+// Detects if device is on iOS 
+const isIos = () => {
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    return /iphone|ipad|ipod/.test( userAgent );
+    }
+    // Detects if device is in standalone mode
+    const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+    // Checks if should display install popup notification:
+    if (isIos() && !isInStandaloneMode()) {
+        document.getElementById("popup").hidden = false;
+    }
+
+function closePopup() {
+    document.getElementById("popup").hidden = true;
+}
