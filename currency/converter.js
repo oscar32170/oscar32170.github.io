05269cfae00715a8b3a5aa424a6de39e0e6e42cb
20011662g <71864080+20011662g@users.noreply.github.com>
2024-05-31 12:56:24 -0400

    Update converter.js

--- currency/converter.js
+++ currency/converter.js
@@ -2,14 +2,16 @@ amount = document.getElementById("amount")
 rate = document.getElementById("rate")
 result = document.getElementById("result")
 
-rate.value = localStorage.getItem("rate")
+window.addEventListener("load", () => {
+    rate.value = localStorage.getItem("rate")
+})
 
-amount.addEventListener("input", function() {
+amount.addEventListener("input", () => {
     var resultValue = convert(amount.value, rate.value);
     result.value = resultValue.toFixed(2);
 });
 
-rate.addEventListener("input", function() {
+rate.addEventListener("input", () => {
     localStorage.setItem("rate", rate.value)
     var resultValue = convert(amount.value, rate.value);
     result.value = resultValue.toFixed(2);
